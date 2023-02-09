c891a8e6f118c25d5f81d101c5e62c4976aa93ad
Michał Pelc <michal.pelc@live.com>
2023-02-09 19:16:13 +0100

    Added outline to the dialog border, and "Save/Cancel" buttons

--- lib/util/new_item_dialog.dart
+++ lib/util/new_item_dialog.dart
@@ -5,6 +5,29 @@ class NewItemDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog();
+    return AlertDialog(
+      backgroundColor: Colors.yellow,
+      content: Container(
+        height: 120,
+        child: Column(
+          children: [
+            // Get user input
+            TextField(
+              decoration: InputDecoration(border: OutlineInputBorder()),
+            ),
+
+            // Save / Cancel buttons
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: null, child: Text("Save")),
+                ElevatedButton(onPressed: null, child: Text("Cancel")),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
